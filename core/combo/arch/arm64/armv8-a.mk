197f5fd5f5534a99b4f5615bf3bbc177163cd9c0
Alex Naidis <alex.naidis@linux.com>
2016-10-07 22:24:07 +0200

    build: condition A53 errata workarounds in RenderScript
    * following the logic I introduced in c546185 and 18e9c5e .
    * CPU targets which are not similar to Cortex-A53 don't need this.
    -> since using the workarounds results in a performance pentalty,
       disable them for targets which don't need them.
    
    Change-Id: I8dff81617f51dd9d63614f9c0306d793dfe6e259
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/combo/arch/arm64/armv8-a.mk
+++ core/combo/arch/arm64/armv8-a.mk
@@ -8,5 +8,6 @@ ifneq (,$(filter cortex-a53 default,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT
 	arch_variant_ldflags := -Wl,--fix-cortex-a53-843419
 else
 	arch_variant_ldflags := -Wl,--no-fix-cortex-a53-843419
+	RS_DISABLE_A53_WORKAROUND := true
 endif
 
